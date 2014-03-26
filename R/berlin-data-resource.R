8facf587dfef6696aa60f2d26c95e7d0f8328c6f
k <kate.a.mccurdy@gmail.com>
2014-03-26 21:49:24 +0100

    start to handle parsing different formats

--- R/berlin-data-resource.R
+++ R/berlin-data-resource.R
@@ -8,7 +8,62 @@ summary.berlin_data_resource <- function(object, ...) {
 #' @method download berlin_data_resource
 #' @param resource a resource of type berlin_data_resource
 download.berlin_data_resource <- function(resource) {
-  NULL
+  stopifnot('format' %in% names(resource), 
+            'url' %in% names(resource))
+  result <- downloadFormat(resource$url, resource$format)
+  result
+}
+
+#' Calls appropriate function for download format
+#' @param url url of file location
+#' @param format string specifying download format, in c("CSV", "JSON", "XML")
+#' @param ... optional additional arguments to download function
+downloadFormat <- function(url, format, ...) {
+  switch(format,
+         CSV = read.csv(url, ...),
+         JSON = downloadJson(url, ...),
+         XML = downloadXml(url, ...))
+}
+
+#' Downloads json file
+#' @param json.url url of json file location
+#' @param parse.to.df logical: should the function try to parse the JSON output into a data.frame?
+#' @param ... optional additional arguments to download function
+downloadJSON <- function(json.url, parse.to.df=TRUE, ...) {
+  require(rjson)
+  result <- fromJSON(file=json.url, ...)
+  if(parse.to.df) {
+    data <- result[[3]]
+    data <- lapply(data, unlist)
+    data <- do.call(rbind, data)
+    result <- data.frame(data) 
+  }
+  result
+}
+
+#' Downloads xml file
+#' @param xml.url url of xml file location
+#' @param parse.to.df logical: should the function try to parse the XML output into a data.frame?
+#' @param ... optional additional arguments to download function
+downloadXML = function(xml.url, parse.to.df=TRUE, ...) {
+  result <- xmlTreeParse(xml.url, ...)
+  if (parse.to.df) {
+    data <- result[[1]][[1]][[3]]
+    # TODO this doesn't work
+    data1 <- lapply(data, 
+                   function(item) {
+                     datarow <- sapply(xmlChildren(item), xmlValue)
+                     if(class(datarow)=="list") datarow <- unlist(datarow)
+                     print(names(datarow)) 
+                     print(names(item))
+                     print(class(datarow))
+                     datarow
+                   })
+    data <- do.call(rbind, data)
+    dimnames()
+    result1 <- data.frame(data)
+  }
+  result
 }
 
 #' Downloads a resource
