483d4e702e42fe022c06cccfe9b967bd857206cc
Dirk Schumacher <mail@dirk-schumacher.net>
2014-04-09 22:07:23 +0200

    fixes a test

--- tests/testthat/test-berlin-data-list.R
+++
@@ -1,8 +0,0 @@
-context("berlin_data_list")
-
-test_that("outputs a summary", {
-  data_list1 <- structure(list(list(title = "data set 1")), 
-                          class = "berlin_data_list")
-  expected_output1 <- list(length = 1, data_sets = c("data set 1"))
-  expect_equivalent(summary(data_list1), expected_output1)
-})
