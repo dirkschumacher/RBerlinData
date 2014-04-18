b78a8826aed853887a2a0120014e366c0427e8f0
k <kate.a.mccurdy@gmail.com>
2014-04-18 19:49:55 +0200

    check out this actually working app!

--- shiny/server.R
+++ shiny/server.R
@@ -1,27 +1,113 @@
 library(shiny)
 library(BerlinData)
 
-data = reactiveValues()
-
 shinyServer(function(input, output) {
 
   queryData = reactive(searchBerlinDatasets(input$query))
   queryDF = reactive(as.data.frame(queryData()))
   
-  #whichdataset = reactive(queryData()[[input$whichdataset]])
+  datasetMetaData = reactive({
+    if(as.numeric(input$which_dataset) == 0 | as.numeric(input$which_dataset) > nrow(queryDF())) 
+      list()
+    else {
+        getDatasetMetaData(
+          paste(queryDF()[as.numeric(input$which_dataset), 'link'])
+          )
+    }})
   
-  output$info = reactive(
-    if(inherits(queryData(), "berlin_data_query_no_results")) {
-      "No results were found for your query; please try again"
-    } else {
-      paste("Found", length(queryData()), "datasets")
-    }
-    )
+  datasetMetaDataDF = reactive({
+    if(as.numeric(input$which_dataset) == 0 | as.numeric(input$which_dataset) > nrow(queryDF())) 
+      data.frame(title=NA, resources=NA)
+    else {
+      as.data.frame(datasetMetaData())   
+    }})
+  
+  dataResource = reactive({
+    if(as.numeric(input$which_resource) == 0 | as.numeric(input$which_resource) > nrow(datasetMetaDataDF())) 
+      data.frame(data=NA)
+    else {
+      download(
+        resources(datasetMetaData())[[as.numeric(input$which_resource)]]
+      )  
+    }})
+  
+  output$query_info = renderText(
+    ifelse(inherits(queryData(), "berlin_data_query_no_results"),
+           "No results were found for your query; please try again",
+           paste0("Found ", length(queryData()), " dataset", 
+                  ifelse(length(queryData()) > 1, "s", ""),".\n",
+                 "Proceed to the Dataset tab to get metadata for any dataset."))
+  )
 
-  output$datasettable = renderTable({
-    queryDF() 
+  output$query_table = renderTable({ queryDF() })
+  
+  output$dataset_info = renderText(
+    ifelse(as.numeric(input$which_dataset) == 0,
+           "No dataset selected",
+           ifelse(as.numeric(input$which_dataset) > nrow(queryDF()),
+                  paste0("Please select ", 
+                         ifelse(nrow(queryDF()) > 1,"one of the ", "the "),
+                        nrow(queryDF()), 
+                        " dataset", ifelse(nrow(queryDF()) > 1,"s", ""),
+                        " in the list"),
+                  paste("Selected dataset #", 
+                        as.numeric(input$which_dataset), "of",
+                        nrow(queryDF()), "found by query.\n",
+                        "Title:", queryDF()$title[[as.numeric(input$which_dataset)]],"\n",
+                        "Found", nrow(datasetMetaDataDF()), "resources in dataset.\n",
+                        "Proceed to the Data Resource tab to download a resource.")))
+  )
+  
+  output$dataset_table = renderTable({ datasetMetaDataDF() })
+
+  output$data_resource_info = renderText({
+    if (as.numeric(input$which_resource) == 0)
+           "No data resource selected"
+    else if(as.numeric(input$which_resource) > nrow(datasetMetaDataDF()))
+                  paste("Please select one of the", 
+                        nrow(datasetMetaDataDF()), 
+                        "data resources in the list")
+    else if(datasetMetaDataDF()$scheme[as.numeric(input$which_resource)] == "https")
+      paste("Selected resource #", 
+            input$which_resource, "of",
+            nrow(datasetMetaDataDF()), "found for dataset.\n",
+            "Title:", queryDF()$title[[as.numeric(input$which_dataset)]],"\n",
+            "URL:", datasetMetaDataDF()$url[[as.numeric(input$which_resource)]],"\n",
+            "Unfortunately, R does not support downloads from an https:// source.")
+    else if(!(datasetMetaDataDF()$format[as.numeric(input$which_resource)] %in% c("CSV", "JSON", "TXT", "XML")))
+      paste("Selected resource #", 
+            input$which_resource, "of",
+            nrow(datasetMetaDataDF()), "found for dataset.\n",
+            "Title:", queryDF()$title[[as.numeric(input$which_dataset)]],"\n",
+            "URL:", datasetMetaDataDF()$url[[as.numeric(input$which_resource)]],"\n",
+            "Format:", datasetMetaDataDF()$format[[as.numeric(input$which_resource)]],"\n",
+            "Unfortunately, BerlinData does not support download of this format.")
+    else if(is.null(dataResource()))
+      paste("Selected resource #", 
+            input$which_resource, "of",
+            nrow(datasetMetaDataDF()), "found for dataset.\n",
+            "Title:", queryDF()$title[[as.numeric(input$which_dataset)]],"\n",
+            "URL:", datasetMetaDataDF()$url[[as.numeric(input$which_resource)]],"\n",
+            "An error occurred in downloading this resource. Please try manual download.")
+    else
+      paste("Selected resource #", 
+            input$which_resource, "of",
+            nrow(datasetMetaDataDF()), "found for dataset.\n",
+            "Title:", queryDF()$title[[as.numeric(input$which_dataset)]],"\n",
+            "URL:", datasetMetaDataDF()$url[[as.numeric(input$which_resource)]],"\n",
+            "Showing", ifelse(nrow(dataResource()) < 6, nrow(dataResource()), 6), 
+            "of", nrow(dataResource()), "rows in this resource.\n",
+            "Click 'Download' to download data resource as a CSV.")
   })
   
-  #output$resourcetable = renderTable({data.frame()})
+  output$data_resource_table = renderTable({ head(dataResource()) })
+  
+  output$download_data_resource = downloadHandler(
+    filename = function() paste0("berlin_data_resource_", 
+                                 gsub('\\s', '-', Sys.time()), ".csv"),
+    content = function(file) {
+      write.csv(dataResource(), file=file)
+    }
+    )
   
 })
--- shiny/ui.R
+++ shiny/ui.R
@@ -1,24 +1,64 @@
 library(shiny)
 
 shinyUI(fluidPage(
-  titlePanel("Berlin Data Explorer"),
-  sidebarLayout(
-    sidebarPanel(
-      textInput(inputId="query", label="Your Query Here:", value="Vornamen"),
-      submitButton("Submit Query") #,
-      #conditionalPanel(
-      #  condition = "nrow(output.datasettable) > 0",
-      #  textInput("whichdataset", "Select Dataset #"),
-      #  submitButton("Get Dataset Metadata")
-      #  )
-      ),
-    mainPanel(
-      verbatimTextOutput(outputId="info"),
-      tableOutput(outputId="datasettable")
-    ) #,
-    #conditionalPanel(
-    #  condition="nrow(output.datasettable) > 0",
-    #  tableOutput(outputId="resourcetable")
-    #)
-    )
-  ))
+  title="Berlin Data Explorer",
+  fluidRow(
+    column(12,
+           tabsetPanel(
+             tabPanel("Query",
+                      column(2,
+                             wellPanel(
+                               textInput(inputId="query", 
+                                         label="Your Query Here:", 
+                                         value="Vornamen"),
+                               submitButton("Search Berlin Datasets") 
+                             )
+                      ),
+                      column(10,
+                             mainPanel(
+                               verbatimTextOutput(outputId="query_info"),
+                               tableOutput(outputId="query_table")
+                             )
+                      )
+             ),
+             tabPanel("Dataset Metadata",
+                      column(2,
+                             wellPanel(
+                               textInput("which_dataset", 
+                                         "Select dataset #\n
+                                           from datasets found by query",
+                                         0),
+                               submitButton("Get Dataset Metadata")
+                             )
+                      ),
+                      column(10,
+                             mainPanel(
+                               verbatimTextOutput(outputId="dataset_info"),
+                               tableOutput(outputId="dataset_table")
+                             )
+                      )
+             ),
+             tabPanel("Data Resource",
+                      column(2,
+                             wellPanel(
+                               textInput("which_resource", 
+                                         "Select resource #\n
+                                         from resources in dataset",
+                                         0),
+                               submitButton("Get Data Resource"),
+                               br(),
+                               downloadButton(outputId="download_data_resource",
+                                              "Download Data Resource")
+                             )
+                             ),
+                      column(10,
+                             mainPanel(
+                               verbatimTextOutput(outputId="data_resource_info"),
+                               tableOutput(outputId="data_resource_table")
+                             )
+                      ))
+           )
+    ) 
+  )
+)
+)
