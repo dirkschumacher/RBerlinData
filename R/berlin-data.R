ca1823672c57bd0ecc4224ac63665eff19c998b0
k <kate.a.mccurdy@gmail.com>
2014-04-30 19:51:44 +0200

    add call to globalVariables

--- R/berlin-data.R
+++ R/berlin-data.R
@@ -13,6 +13,9 @@ NULL
 #' @name cached_datasets_feed
 NULL
 
+# globalVariables call to appease R CMD check
+globalVariables('cached_datasets_feed')
+
 ## Generic functions ##
 
 #' Gets metadata for a dataset or list of datasets
@@ -200,7 +203,7 @@ searchData <- function(query,
                            error=function(e) {
                              if (xml.url=="http://daten.berlin.de/datensaetze/rss.xml") {
                                message('Could not establish connection to daten.berlin.de')
-                               data(cached_datasets_feed, envir = environment())
+                               data(cached_datasets_feed)
                                message('Using stored list of Berlin datasets')
                                message(paste0('Last updated: ', attr(cached_datasets_feed, 'last_updated')))
                                datasets_feed = xmlParse(cached_datasets_feed)
